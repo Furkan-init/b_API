7c4d5df3767b35620412b4a499564e19745eae4d
furkan-init <furkanerkanssn1@gmail.com>
2021-03-19 03:17:18 +0300

    Final version of project

--- hw1.js
+++ hw1.js
@@ -1,13 +1,24 @@
+//------------------------------------------------
+//Title: hw1
+//Author: Furkan Erkan
+//Description: This project defines REST Api for cloud app
+//------------------------------------------------
+
+//Adds mongoose package to connect mongoDB
 const mongoose=require('mongoose');
+//Adds express package to use CRUD methods in backend
 const express=require('express');
 const app = express();
 
+//MongoDB connection for localhost
+//Left for test purposes
 /*
 mongoose.connect('mongodb://localhost/computing')
     .then(() => console.log('Connected ...'))
     .catch(err => console.error('Could not', err));
 */
 
+//MongoDB connection for MongoDB Atlas
 mongoose.connect(
     'mongodb+srv://furkan:ymzYaEvmdnCifrPF@cluster0.7uyyg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true},
@@ -17,6 +28,8 @@ mongoose.connect(
     }
 );
 
+
+//Declares schema for database
 const userSchema = new mongoose.Schema({
     id: {type : Number, required: true},
     name: {type : String, required: true},
@@ -24,10 +37,10 @@ const userSchema = new mongoose.Schema({
     email: String,
     age: Number
 });
-
+//Initializes User table which based on schema that has already created
 const User = mongoose.model('User', userSchema);
 
-
+//Creates user object by using given parameters
 async function createUser(id,name, surname, email, age){
     const user = new User({
         id:id,
@@ -38,7 +51,8 @@ async function createUser(id,name, surname, email, age){
     });
     const result = await user.save();
 }
-
+//Creates 3 initial users if there is no user in database
+//Mainly used for if every user delete from the database initialize from scracth for testing
 async function initFirst(){
     const users = await User
     .find();
@@ -48,11 +62,13 @@ async function initFirst(){
     createUser(3,'Baron','Afanas','brafanas@outlook.com',75);
     }
 }
+//Calls initFirst function
 initFirst();  
-//To use req.body
+
+//To use req.body and req.params
 app.use(express.json());
 
-
+//Gets all users from database and sends as response
 app.get("/api/user", (req,res) => {
     async function getUsers(){
         const users = await User
@@ -61,11 +77,14 @@ app.get("/api/user", (req,res) => {
     }
     getUsers();
 });
-
+//Gets all users with specific from database and sends as response
 app.get("/api/user/:id", (req,res) => {
+    //Controls if parameter of request is integer or not
+    //If it is not then sends HTTP 400 error
     if(isNaN(req.params.id)){
         res.status(400).send(`HTTP 400 -- Bad Request`);
     }else {
+        //Controls if there is a user that hasn't already exist sends HTTP 404 error
         async function getSpecific(){
             const users = await User
             .find({id:req.params.id});
@@ -78,8 +97,9 @@ app.get("/api/user/:id", (req,res) => {
         getSpecific();
     }
 });
+//Adds user by using given parameters
 app.post("/api/user", (req,res) => {   
-
+//Controls if there is a user that has already exist sends HTTP 400 error
     async function ctrl(){
         const users = await User
             .find({id:req.body.id});
@@ -89,6 +109,8 @@ app.post("/api/user", (req,res) => {
             res.status(400).send(`HTTP 400 -- Bad Request ~Same User in DB`);
     }
     ctrl();
+    //Controls if there is a request that has missing parameters sends HTTP 400 error
+    //Adds users
     async function addNew(){
         try {
             const users = await createUser(req.body.id,req.body.name,
@@ -101,28 +123,52 @@ app.post("/api/user", (req,res) => {
     }
     
 });
+//Updates user by using given parameters
 app.put("/api/user/:id", (req,res) => {
+    //Controls if parameter of request is integer or not
+    //If it is not then sends HTTP 400 error
     if(isNaN(req.params.id)){
         res.status(400).send(`HTTP 400 -- Bad Request`);
     }
-    async function updateUser(){
-        const user = await User
-        .updateOne({id:req.params.id},{$set: req.body });
-        if(user.nModified > 0 ) 
-            res.send(req.body);
-            else
-            res.status(404).send(`HTTP 404 -- Not Found`);
+    //Controls if there is a user that has already exist sends HTTP 404 error
+    //Updates user
+        async function updateUser(){
+            const user = await User
+            .updateOne({id:req.params.id},{$set: req.body });
+            if(user.nModified > 0 ) 
+                res.send(req.body);
+                else
+                res.status(404).send(`HTTP 404 -- Not Found`);
+        
     }
-   
     updateUser();
 });
 app.delete("/api/user/:id", (req,res) => {
+    //Controls if parameter of request is integer or not
+    //If it is not then sends HTTP 400 error
+    if(isNaN(req.params.id)){
+        res.status(400).send(`HTTP 400 -- Bad Request`);
+    }else {
+         //Controls if there is a user that has already exist sends HTTP 404 error
+        async function ctrl2(){
+            const users = await User
+            .find({id:req.params.id});
+            if(users.length===0){
+                res.status(404).send(`HTTP 404 -- Not Found`);
+            } else{
+                deleteUser();
+                res.send(req.body);
+            }
+            
+        }
+        ctrl2();
+    }
+     //Deletes user
     async function deleteUser(){
         const user = await User
         .deleteOne({id:req.params.id});
     }
-    res.send(req.body);
-   deleteUser();
 });
-
+//Listens if there is a environmental port
+//If not, listens on port 3000 locally
 app.listen(process.env.PORT || 3000,() => console.log('Listening on port 3000...'));
